2bdd6ad31f2200d935011851ccf92d1cc2ac6ded
Antoni Seba <antoniwoj@o2.pl>
2023-02-14 21:25:26 +0100

    init

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Flask tts application
+Simple python app using flask witch allows to generate audio file from text
+
+# Local server
+In terminal type python tts.py
---
+++ tts.py
@@ -0,0 +1,21 @@
+from TTS.api import TTS
+from flask import Flask, request, jsonify, render_template
+import secrets
+
+app = Flask(__name__)
+
+@app.route('/tts', methods=['GET', 'POST'])
+def tts():
+  model_name = TTS.list_models()[0]
+  tts = TTS(model_name)
+  if request.method == 'POST':
+    random_string = secrets.token_hex(20)
+    tts.tts_to_file(text=request.form['text'], speaker=tts.speakers[int(request.form['speaker'])], language=tts.languages[int(request.form['language'])], file_path="static/" + random_string + ".wav")
+    
+    return { "status": "success", "file": "/static/" + random_string + ".wav" }
+  else:
+    return render_template('index.html', languages=tts.languages, speakers=tts.speakers, languages_len=len(tts.languages), speakers_len=len(tts.speakers))
+
+
+if __name__ == '__main__':
+  app.run(host='127.0.0.1', port=5000, debug=True)
